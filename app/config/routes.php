aabc7f2b34e2be339d5d4bec27d94eb2722e7358
Jefferson <161782609+Jcarable04@users.noreply.github.com>
2025-09-11 19:09:32 -0700

    okay

--- app/config/routes.php
+++ app/config/routes.php
@@ -46,5 +46,5 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
 $router->get('/', 'StudentsController::get_all');
 $router->match('/students/create_new', 'StudentsController::create', ['GET', 'POST']);
-$router->match('/students/update_students/{$id}', 'StudentsController::update', ['GET', 'POST']);
+$router->match('/students/update/{$id}', 'StudentsController::update', ['GET', 'POST']);
 $router->get('/students/delete/{$id}', 'StudentsController::delete');
