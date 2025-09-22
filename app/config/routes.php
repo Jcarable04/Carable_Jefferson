0914bfd99680b73d31a07ae627ed50f875554e2c
Jefferson <161782609+Jcarable04@users.noreply.github.com>
2025-09-21 21:13:25 -0700

    k

--- app/config/routes.php
+++ app/config/routes.php
@@ -43,7 +43,8 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 |
 */
 
-$router->get('/', 'StudentsController::get_all');
+$router->get('/', 'StudentsController::index'); // homepage with list, search, pagination
+$router->get('/students', 'StudentsController::index'); // optional, same as above
 $router->match('/students/create_new', 'StudentsController::create', ['GET', 'POST']);
-$router->match('/students/update/{$id}', 'StudentsController::update', ['GET', 'POST']);
-$router->get('/students/delete/{$id}', 'StudentsController::delete');
+$router->match('/students/update/{id}', 'StudentsController::update', ['GET', 'POST']);
+$router->get('/students/delete/{id}', 'StudentsController::delete');
